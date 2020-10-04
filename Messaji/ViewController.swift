d1e0cd58da96c73b1dc38a40302a57379c6c54d2
taiga <matsuotaiga.miraidenshi@gmail.com>
2020-10-05 06:26:54 +0900

    Initial Commit

---
+++ Messaji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messaji
+//
+//  Created by 松尾大雅 on 2020/10/05.
+//  Copyright © 2020 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

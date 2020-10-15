b46fc7aa6d5e40d4930106682d8c111bd9a46010
taiga <matsuotaiga.miraidenshi@gmail.com>
2020-10-15 15:55:07 +0900

    10/14

--- Messaji/Controller/AppDelegate.swift
+++ Messaji/Controller/AppDelegate.swift
@@ -7,16 +7,33 @@
 //
 
 import UIKit
+import os
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+       // 通知許可の取得
+        UNUserNotificationCenter.current().requestAuthorization(
+        options: [.alert, .sound, .badge]){
+            (granted, _) in
+            if granted{
+                UNUserNotificationCenter.current().delegate = self
+            }
+        }
         return true
     }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+           willPresent notification: UNNotification,
+           withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    
+           os_log("Notified")
+           // アプリ起動時も通知を行う
+           completionHandler([.sound, .alert ])
+       }
 
     // MARK: UISceneSession Lifecycle
 
@@ -34,4 +51,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- Messaji/Controller/ViewController.swift
+++ Messaji/Controller/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwiftKeychainWrapper
+import os
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
@@ -15,49 +16,95 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
        @IBOutlet weak var EmailText: UITextField!
        @IBOutlet weak var NameText: UITextField!
        @IBOutlet weak var TextView: UITextView!
-    //置いとくだけ
+       @IBOutlet weak var DateLabel: UILabel!
+       @IBOutlet var DateLabel2 : UILabel!
        @IBOutlet weak var E: UIImageView!
        @IBOutlet weak var N: UIImageView!
     
        var saveData : UserDefaults = UserDefaults.standard
        var Userimage : UIImage!
+       var request:UNNotificationRequest!
       
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //judgeDate()
         // 画像をタップすることを可能に
         self.AddImage.isUserInteractionEnabled = true
-        //userdefaultsの定義1
-      
+        //userdefaultsの定義
         EmailText.text = saveData.object(forKey: "key_EmailText") as? String
         NameText.text  = saveData.object(forKey: "key_NameText") as? String
         TextView.text  = saveData.object(forKey: "key_TextView") as? String
         
-
-       let ImageData = saveData.data(forKey: "key_AddImage")
+         if saveData.object(forKey: "key_AddImage") != nil {
+        let ImageData = saveData.data(forKey: "key_AddImage")
        // Data型からUIImage型へ変換
-       let Userimage2 = UIImage(data: ImageData!)
-       Userimage = Userimage2
-       AddImage.image = Userimage
+        let Userimage2 = UIImage(data: ImageData!)
+        Userimage = Userimage2
+        AddImage.image = Userimage
+        }
         
-    }
-         
-
-    //登録ボタンで値をuserdefaultsへ保存する
-        @IBAction func save() {
-        print("登録しました")
-       
-        saveData.set(EmailText.text, forKey: "key_EmailText")
-        saveData.set(NameText.text, forKey: "key_NameText")
-        saveData.set(TextView.text, forKey: "key_TextView")
         
-       let data = Userimage.pngData()
-       saveData.set(data, forKey: "key_AddImage")
-
     }
     
+    func judgeDate(){
+        
+    //現在のカレンダ情報を設定
+    let calender = Calendar.current
+    //日本時間を設定
+    let now_day = Date(timeIntervalSinceNow: 60 * 60 * 9)
+    //日付判定結果
+    var judge = Bool()
+    // 日時経過チェック
+    if saveData.object(forKey: "today") != nil {
+         let past_day = saveData.object(forKey: "today") as! Date
+         let now = calender.component(.minute, from: now_day)
+         let past = calender.component(.minute, from: past_day)
+         let diff = calender.dateComponents([.minute], from: past_day, to: now_day)
+         print(diff.minute!)
+         DateLabel.text = String(diff.minute!)
+    
+        //日にちが変わっていた場合
+                 if now != past {
+                    judge = true
+        
+                 }
+                 else {
+                    judge = false
+                   
+                 }
+             }
+             //初回実行のみelse(nilならば、)
+             else {
+                 judge = true
+                 /* 今の日時を保存 */
+                 saveData.set(now_day, forKey: "today")
+                 
+             }
+
+             /* 日付が変わった場合はtrueの処理 */
+             if judge == true {
+                  judge = false
+                //日付が変わった時の処理
+               
+             }
+             else {
+              //日付が変わっていない時の処理をここに書く
+                DateLabel.text = "0"
+             }
+    }
+         
+    //登録ボタンで値をuserdefaultsへ保存する
+        @IBAction func save() {
+            
+            saveData.set(EmailText.text, forKey: "key_EmailText")
+            saveData.set(NameText.text, forKey: "key_NameText")
+            saveData.set(TextView.text, forKey: "key_TextView")
+            let data = Userimage.pngData()
+            saveData.set(data, forKey: "key_AddImage")
+            judgeDate()
+    }
     
       // タップされたときの処理
         @IBAction func tapped(sender: UITapGestureRecognizer){
@@ -81,6 +128,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             alert.addAction(UIAlertAction(title:"Cancel",style: .default, handler: nil))
             present(alert,animated: true, completion: nil)
         }
+    
+    
+
 }
     
 
